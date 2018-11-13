9cc49369cf3aaf28b4c8d9ce38c998001f164a36
gabrieldemarmiesse <gabreil.de-marmiesse@valeo.com>
2018-11-13 11:40:32 +0100

    Making the library installable with pip.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # dl_utils
 The library used in the Valeo Deep learning training.
+
+Install it with
+```
+pip install .
+```
+
+or if you want to work on it (dev mode, will not make a copy in site-packages)
+```
+pip install -e .
+```
+
+Uninstall with 
+```
+pip uninstall dl-utils
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(name='dl-utils',
+      version='0.1',
+      description='Valeo\'s library for Deep learning training',
+      author='Valeo',
+      install_requires=['numpy>=1.9.1'],
+      packages=find_packages())
