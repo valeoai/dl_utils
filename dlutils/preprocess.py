014eb40ea55eb54b9176aab6fc1842e97f6406ae
jrebut <julien.rebut@valeo.com>
2018-11-29 13:46:14 -0500

    unpreprocess is now a symetry of preprocess

--- dlutils/preprocess.py
+++ dlutils/preprocess.py
@@ -1,18 +1,82 @@
 import numpy as np
 
+def unpreprocess(x, data_format,mode):
+    """unpreprocesses a Numpy array encoding a batch of images.
+    # Arguments
+        x: Input array, 3D or 4D.
+        data_format: Data format of the image array.
+        mode: One of "caffe", "tf" or "torch".
+            - caffe: will convert the images from RGB to BGR,
+                then will zero-center each color channel with
+                respect to the ImageNet dataset,
+                without scaling.
+            - tf: will scale pixels between -1 and 1,
+                sample-wise.
+            - torch: will scale pixels between 0 and 1 and then
+                will normalize each channel with respect to the
+                ImageNet dataset.
+    # Returns
+        unreprocessed Numpy array.
+    """
+    if not issubclass(x.dtype.type, np.floating):
+        x = x.astype(backend.floatx(), copy=False)
 
-def unpreprocess(im, mode):
     if mode == 'tf':
-        tmp = (im + 1) * 127.5
-        return tmp.astype(int)
+        x += 1.
+        x *= 127.5
+        np.clip(x / 225., 0, 1)
+        x = x.astype(int)
+        return x
+
+    if mode == 'torch':
+        mean = [0.485, 0.456, 0.406]
+        std = [0.229, 0.224, 0.225]
     else:
         mean = [103.939, 116.779, 123.68]
+        std = None
 
-        im_cp = np.copy(im)
-        im_cp[:, :, 0] += mean[0]
-        im_cp[:, :, 1] += mean[1]
-        im_cp[:, :, 2] += mean[2]
+    # Zero-center by mean pixel
+    if data_format == 'channels_first':
+        if x.ndim == 3:
+            if std is not None:
+                x[0, :, :] *= std[0]
+                x[1, :, :] *= std[1]
+                x[2, :, :] *= std[2]
+                
+            x[0, :, :] += mean[0]
+            x[1, :, :] += mean[1]
+            x[2, :, :] += mean[2]
 
-        im_cp = im_cp[..., ::-1]
+        else:
+            if std is not None:
+                x[:, 0, :, :] *= std[0]
+                x[:, 1, :, :] *= std[1]
+                x[:, 2, :, :] *= std[2]
+                
+            x[:, 0, :, :] += mean[0]
+            x[:, 1, :, :] += mean[1]
+            x[:, 2, :, :] += mean[2]
 
-        return np.clip(im_cp / 225., 0, 1)
+    else:
+        if std is not None:
+            x[..., 0] *= std[0]
+            x[..., 1] *= std[1]
+            x[..., 2] *= std[2]        
+        x[..., 0] += mean[0]
+        x[..., 1] += mean[1]
+        x[..., 2] += mean[2]
+
+    if mode == 'torch':
+        x *= 255.
+    else:
+        if data_format == 'channels_first':
+            # 'RGB'->'BGR'
+            if x.ndim == 3:
+                x = x[::-1, ...]
+            else:
+                x = x[:, ::-1, ...]
+        else:
+            # 'RGB'->'BGR'
+            x = x[..., ::-1]
+            
+    return x
