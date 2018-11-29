b76f3c0dd3b3f5170ad2b9472cd5f1ec5fc839da
jrebut <julien.rebut@valeo.com>
2018-11-29 13:48:57 -0500

    add multi_object class + updated create_subdataset fct
    - add multi_object class
    - create_subdataset modified to check first if dir struct already exist so that it avoids to copy/past again all the data if already done

--- dlutils/dataset.py
+++ dlutils/dataset.py
@@ -7,6 +7,13 @@ from PIL import Image
 
 
 def create_subdataset(path):
+
+    if(os.path.exists(path / 'subdataset/train/dogs') & 
+       os.path.exists(path / 'subdataset/train/cats') & 
+       os.path.exists(path / 'subdataset/valid/dogs') & 
+       os.path.exists(path / 'subdataset/valid/cats')):
+        return
+
     os.makedirs(path / 'subdataset/train/dogs')
     os.makedirs(path / 'subdataset/train/cats')
     os.makedirs(path / 'subdataset/valid/dogs')
@@ -73,16 +80,6 @@ class GeneratorSingleObject(keras.utils.Sequence):
 
         return X, y
 
-    def next_item(self, index):
-        """Generate one batch of data"""
-        # Generate indexes of the batch
-        indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
-
-        # Generate data
-        X, y = self.__data_generation(indexes)
-
-        return X, y
-
     def on_epoch_end(self):
         """Updates indexes after each epoch"""
         self.indexes = np.arange(len(self.df))
@@ -117,4 +114,91 @@ class GeneratorSingleObject(keras.utils.Sequence):
 
         Y_clas = keras.utils.to_categorical(Y_clas, self.NbClasses)
 
-        return X, [Y_bb, Y_clas]
+        return X, [Y_bb,Y_clas]
+    
+class Generator_MultiObject(keras.utils.Sequence):
+    'Generates data from a Dataframe'
+    def __init__(self, df, folder,preprocess_fct,batch_size=32, dim=(32,32), shuffle=True):
+        'Initialization'
+        self.preprocess_fct = preprocess_fct
+        self.dim = dim
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.folder = folder
+
+        self.df = df
+        self.n = len(df)            
+        self.nb_iteration = int(np.floor(self.n  / self.batch_size))
+        
+        self.on_epoch_end()
+                    
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return self.nb_iteration
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+
+        # Generate data
+        X, y = self.__data_generation(indexes)
+
+        return X, y
+   
+    def on_epoch_end(self):
+        'Updates indexes after each epoch'
+        self.indexes = np.arange(len(self.df))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation(self, index):
+        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
+        # Initialization
+        nb_label_max = 20
+        X = np.empty((self.batch_size, *self.dim, 3))
+        Y = []
+        
+        # Generate data
+        for i, ID in enumerate(index):
+            # Read the image
+            img = Image.open(self.folder/self.df['filename'][ID])
+            
+            # extract the number of label
+            c = self.df['class'][ID]
+            nb_label = len(c)            
+            
+            # Class in a form of a one hot encoding
+            y = np.zeros((nb_label_max,1+4));
+            y[:nb_label,0] = c
+            
+            # reshape the bounding box and resize
+            bbox = np.asmatrix(self.df['bbox'][ID])
+            bbox = bbox.reshape(nb_label,4)
+            
+            bbox_rescaled = np.copy(bbox)    
+            bbox_rescaled = bbox_rescaled.astype(float)
+            width, height = img.size
+            RatioX = width/self.dim[0]
+            RatioY = height/self.dim[1]
+
+            bbox_rescaled[:,0] = bbox_rescaled[:,0]/RatioY/self.dim[1]
+            bbox_rescaled[:,1] = bbox_rescaled[:,1]/RatioX/self.dim[0]
+            bbox_rescaled[:,2] = bbox_rescaled[:,2]/RatioY/self.dim[1]
+            bbox_rescaled[:,3] = bbox_rescaled[:,3]/RatioX/self.dim[0]        
+        
+            # save the bb coordinates
+            y[:nb_label,1:5] = bbox_rescaled
+            
+       
+            # reshape to a vector
+            y=np.reshape(y,nb_label_max*5)
+                                                                    
+            img = np.asarray(img.resize(self.dim))
+            X[i,] = self.preprocess_fct(np.asarray(img))
+            
+            Y.append(np.asarray(y))
+
+        Y = np.asarray(Y)
+        
+        return X, Y 
